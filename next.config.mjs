ecc7eda1ee59bcf6bff1da0a6036417bea66b418
Hizwan Zameri <hizwan@fulkrum.net>
2025-01-18 15:38:25 +0800

    update package.json and next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Specify static export
     output: 'export',
-    // Optional: Enable additional configuration as needed
-    trailingSlash: true, // Ensure paths have trailing slashes for static hosting
 };
 
 export default nextConfig;
