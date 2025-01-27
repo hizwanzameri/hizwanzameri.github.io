51710aec3bc1cdd028eb096a91e0602b27ba0b0b
Hizwan Zameri <hizwan@fulkrum.net>
2025-01-27 17:07:27 +0800

    update info

--- app/components/navigation.js
+++ app/components/navigation.js
@@ -1,5 +1,14 @@
+const MenuItem = ({ children }) => {
+    return (
+        <div className="rounded-full border border-white/20 px-4 py-2 text-sm">{children}</div>
+    )
+}
+
 export default function Navigation() {
     return (
-        <div className="fixed bottom-10">Test</div>
+        <div className="fixed bottom-10 flex flex-row gap-3">
+            <MenuItem>My Profile</MenuItem>
+            <MenuItem>Technology & Information System - Assignments & Reflection</MenuItem>
+        </div>
     )
 }
--- app/page.js
+++ app/page.js
@@ -5,7 +5,15 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <p className="text-7xl">Hizwan Zameri</p>
+        <Image
+          aria-hidden
+          src="/profile-1.png"
+          alt="profile"
+          className="rounded-full"
+          width={150}
+          height={150}
+        />
+        <p className="bg-gradient-to-b from-[#fff] from-30%  to-[#fff]/30 text-7xl bg-clip-text text-transparent drop-shadow-[0_20px_35px_rgba(255,255,255,0.6)]">Hizwan Zameri</p>
         <span>I'm a UI/UX Developer.</span>
         {/* <Image
           className="dark:invert"
@@ -19,91 +27,28 @@ export default function Home() {
           <li className="mb-2">
             Welcome to my portfolio{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              hizwanzameri.github.io
+              <a
+                className=""
+                href="https://hizwanzameri.github.io"
+                target="_blank"
+                rel="noopener noreferrer"
+              >hizwanzameri.github.io</a>
             </code>
             .
           </li>
           <li>Visit my github profile{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              github.com/hizwanzameri
+              <a
+                className=""
+                href="https://github.com/hizwanzameri"
+                target="_blank"
+                rel="noopener noreferrer"
+              >github.com/hizwanzameri</a>
             </code>
             .
           </li>
         </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
       <Navigation />
     </div>
   );
